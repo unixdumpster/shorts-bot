df24bd0d29380062f2c5645bae38c23e3f99e7b1
unknown <unixdumpster@gmail.com>
2023-11-20 16:28:19 -0600

    add polly support

--- driver.py
+++ driver.py
@@ -1,4 +1,5 @@
 import proxies.ffmpeg_proxy as ffmpeg
+import proxies.pollytts_proxy as pollytts
 import proxies.gtts_proxy as tts
 import proxies.reddit_proxy as reddit
 import proxies.youtube_proxy as yt
@@ -8,6 +9,7 @@ reddit_client = reddit.get_reddit_client(reddit.get_token_parameters())
 
 posts = reddit.get_posts("AmITheAsshole", reddit_client, 10)
 post = list(posts)[0]
-tts.create_audio(pr.proofread(post.title), pr.proofread(post.selftext))
-yt.download_video("https://www.youtube.com/watch?v=YwILPdVZsIY")
-ffmpeg.merge_audio_and_video("video.mp4", "output.wav", "result.mp4")
+pollytts.create_audio(pr.proofread(post.title), pr.proofread(post.selftext))
+# tts.create_audio(pr.proofread(post.title), pr.proofread(post.selftext))
+# yt.download_video("https://www.youtube.com/watch?v=YwILPdVZsIY")
+# ffmpeg.merge_audio_and_video("video.mp4", "output.wav", "result.mp4")
