5cb420e4b9c9c1d42fe027a006b1282ac6acb5e9
unknown <unixdumpster@gmail.com>
2023-11-20 12:26:28 -0600

    add env for path config

--- raw_videos_proxy.py
+++ raw_videos_proxy.py
@@ -1,12 +1,14 @@
-import sys
+import os
 import pytube
+from dotenv import load_dotenv
 from time import time
 
-# hm_videos = {'CSGO': 'https://www.youtube.com/watch?v=Lixl3-jz7k8', 
-#                 'MC': 'https://www.youtube.com/watch?v=intRX7BRA90', 
-#                 'GTA': 'https://www.youtube.com/watch?v=VB0zDS9ITmk'}
+load_dotenv()
 
-hm_videos = {'GTA': 'https://www.youtube.com/watch?v=jhFDyDgMVUI'}
+hm_videos = {'CSGO': 'https://www.youtube.com/watch?v=Lixl3-jz7k8', 
+                'MC': 'https://www.youtube.com/watch?v=intRX7BRA90', 
+                'GTA': 'https://www.youtube.com/watch?v=VB0zDS9ITmk'}
+
 titles = hm_videos.keys()
 urls = hm_videos.values()
 
@@ -14,8 +16,8 @@ start = time()
 for title, url in zip(titles, urls):
     yt = pytube.YouTube(url)
     video = yt.streams.get_highest_resolution()
-    path = "\\raw-videos"
-    video.download(filename=f"{title}.mp4")
+    path = os.getcwd() + os.getenv("RAW_VIDEOS_DIR")
+    video.download(output_path = path, filename=f"{title}.mp4")
 end = time()
 
-# print("Total seconds elapsed: ", end - start)
+print("Total seconds elapsed: ", end - start)
