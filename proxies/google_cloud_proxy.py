262dd39f2048960b4a4a289dce523abc6bf57822
randyyu13 <ldyu999@gmail.com>
2023-11-23 12:38:38 -0600

    fmt

--- proxies/google_cloud_proxy.py
+++ proxies/google_cloud_proxy.py
@@ -21,7 +21,7 @@ def upload_blob(bucket_name, audio_path, destination_path):
     )
     return f"gs://{bucket_name}/{destination_path}"
     
-
+# get the google cloud speech to text response object
 def long_running_recognize(storage_uri):
     client_file = 'gcs_creds.json'
     client = speech.SpeechClient(credentials=get_credentials(client_file))
@@ -44,23 +44,30 @@ def long_running_recognize(storage_uri):
 
     return result
 
+# get the transcript that will be processed in our video creation
 def get_transcript(gcs_response, bin_size=0.5):
+    # gcs_response is the json response from the speech-to-text api call
+    # bin_size is the interval in seconds in which we want to split the response into
     transcript = []
     current_sentence = []
     current_sentence_start = None
     current_sentence_end = None
 
     for result in gcs_response.results:
+        # google responses have alternative transcriptions in which we selected the one the model is most confident in (alternatives[0])
         alternative = result.alternatives[0]
         for word_info in alternative.words:
+            # info we have from the json response
             word = word_info.word
             start_time = word_info.start_time
             end_time = word_info.end_time
 
+            # is this the first sentence? if so, need to instantiate the current_sentence start and end times.
             if current_sentence_start is None:
                 current_sentence_start = start_time
                 current_sentence_end = start_time
 
+            # does current word belong in this bin or next one?
             if current_sentence_end.total_seconds() - current_sentence_start.total_seconds() > bin_size:
                 transcript.append(((current_sentence_start.total_seconds(), current_sentence_end.total_seconds()), " ".join(current_sentence)))
                 current_sentence = []
