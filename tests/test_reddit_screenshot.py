730cc02aa76db0a1943ea9368235d6a6ea6e632b
Charles Cao <ccao2776@gmail.com>
2023-11-22 19:21:27 -0600

    add tests

--- tests/test_reddit_screenshot.py
+++ tests/test_reddit_screenshot.py
@@ -3,15 +3,16 @@ import unittest
 from playwright.sync_api import sync_playwright
 from dotenv import load_dotenv
 
-class TestStringMethods(unittest.TestCase):
+class TestScreenshot(unittest.TestCase):
         
     def test_is_returned(self):
         with sync_playwright() as p:
             browser = p.chromium.launch()
             page = browser.new_page()
-            page.goto('https://www.reddit.com/r/vegas/comments/180a9mo/psa_avoid_using_cabs_like_the_plague_if_you_are/')
-            
-            page.screenshot(path="screenshot.png")
+            page.goto('https://www.reddit.com/r/confessions/comments/181gg06/my_mom_unknowingly_destroyed_my_porn_stash/')
+            page.wait_for_load_state()
+            page.wait_for_selector('shreddit-post')
+            page.locator('shreddit-post').screenshot(path="scr3eenshot.png")
 
 if __name__ == '__main__':
     unittest.main()
