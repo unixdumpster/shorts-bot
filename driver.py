273ee440601a2ac2bf4b745588b1396d2d1303a6
randyyu13 <ldyu999@gmail.com>
2023-11-28 03:06:46 -0600

    fmt

--- driver.py
+++ driver.py
@@ -5,7 +5,7 @@ import os
 import random as rand
 import proxies.google_cloud_proxy as gcp
 from dotenv import load_dotenv
-import utils.proofread as pr 
+import utils.parse_content as pc
 
 load_dotenv()
 # content
@@ -15,24 +15,27 @@ reddit_client = RedditClient(os.getenv('REDDIT_CLIENT_ID'),
 
 post = list(reddit_client.get_posts_and_screenshots(1).keys())[0]
 
-# audio
-audio_path = os.path.join(os.getcwd(), 'raw_audios')
-audio_title = pollytts.create_audio(pr.proofread(post.title), pr.proofread(post.selftext)[:950])
-audio_path = os.path.join(audio_path, audio_title)
+# split post into multiple tuples of titles and texts
+story_parts = pc.split_content(post, 180)
+for part in story_parts:
+    # audio
+    audio_path = os.path.join(os.getcwd(), 'raw_audios')
+    audio_title = pollytts.create_audio(pc.proofread(part[0]), pc.proofread(part[1]))
+    audio_path = os.path.join(audio_path, audio_title)
 
-# subtitles
-upload_audio_and_get_uri = gcp.upload_blob("shorts-bot-audio-bucket", audio_path, "audios/temp")
-gcs_response = gcp.long_running_recognize(upload_audio_and_get_uri)
-subtitles = gcp.get_transcript(gcs_response, 0.3)
+    # subtitles
+    upload_audio_and_get_uri = gcp.upload_blob("shorts-bot-audio-bucket", audio_path, "audios/temp")
+    gcs_response = gcp.long_running_recognize(upload_audio_and_get_uri)
+    subtitles = gcp.get_transcript(gcs_response, 0.3)
 
-# video
-video_path = os.path.join(os.getcwd(), 'raw_videos')
-videos = os.listdir(video_path)
-video_type = videos[rand.randint(0, 2)]
-video_path = os.path.join(video_path, video_type)
+    # video
+    video_path = os.path.join(os.getcwd(), 'raw_videos')
+    videos = os.listdir(video_path)
+    video_type = videos[rand.randint(0, 2)]
+    video_path = os.path.join(video_path, video_type)
 
-# product
-final_title = video_type.split(".")[0] + "_" + audio_title.split(".")[0].strip(" ") + "_FINAL.mp4"
-print(final_title)
-final_path = os.path.join('final_products', final_title)
-vb.generate_video(video_path, audio_path, subtitles, vb.generate_screenshot_overlay("screenshot.png", 3.5, video_path), os.path.join('final_products', final_title))
+    # product
+    final_title = video_type.split(".")[0] + "_" + audio_title.split(".")[0].strip(" ") + "_FINAL.mp4"
+    print(final_title)
+    final_path = os.path.join('final_products', final_title)
+    vb.generate_video(video_path, audio_path, subtitles, vb.generate_screenshot_overlay("screenshot.png", 3.5, video_path), os.path.join('final_products', final_title))
