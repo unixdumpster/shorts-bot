854f14a816f5b736ed93795282d8b2104724e13f
Charles Cao <ccao2776@gmail.com>
2023-11-30 17:44:51 -0600

    add initial setup and cleanup

--- driver.py
+++ driver.py
@@ -6,8 +6,22 @@ import random as rand
 import proxies.google_cloud_proxy as gcp
 from dotenv import load_dotenv
 import utils.parse_content as pc
+import utils.generate_videos as gv
 
+# setup
 load_dotenv()
+
+raw_video_path = os.path.join(os.getcwd(), 'raw_videos')
+if not os.path.exists(raw_video_path):
+    os.mkdir(raw_video_path)
+    gv.generate_videos(raw_video_path)
+
+raw_audio_path = os.path.join(os.getcwd(), 'raw_audios')
+try:
+    os.mkdir(raw_audio_path)
+except OSError as error:
+    print(error)
+
 # content
 reddit_client = RedditClient(os.getenv('REDDIT_CLIENT_ID'),
                              os.getenv('REDDIT_CLIENT_SECRET'),
@@ -19,9 +33,8 @@ post = list(reddit_client.get_posts_and_screenshots(1).keys())[0]
 story_parts = pc.split_content(post, 180)
 for part in story_parts:
     # audio
-    audio_path = os.path.join(os.getcwd(), 'raw_audios')
     audio_title = pollytts.create_audio(pc.proofread(part[0]), pc.proofread(part[1]))
-    audio_path = os.path.join(audio_path, audio_title)
+    audio_path = os.path.join(raw_audio_path, audio_title)
 
     # subtitles
     upload_audio_and_get_uri = gcp.upload_blob("shorts-bot-audio-bucket", audio_path, "audios/temp")
@@ -29,13 +42,14 @@ for part in story_parts:
     subtitles = gcp.get_transcript(gcs_response, 0.3)
 
     # video
-    video_path = os.path.join(os.getcwd(), 'raw_videos')
-    videos = os.listdir(video_path)
+    videos = os.listdir(raw_video_path)
     video_type = videos[rand.randint(0, 2)]
-    video_path = os.path.join(video_path, video_type)
+    video_path = os.path.join(raw_video_path, video_type)
 
     # product
     final_title = video_type.split(".")[0] + "_" + audio_title.split(".")[0].strip(" ") + "_FINAL.mp4"
     print(final_title)
     final_path = os.path.join('final_products', final_title)
     vb.generate_video(video_path, audio_path, subtitles, vb.generate_screenshot_overlay("screenshot.png", 3.5, video_path), os.path.join('final_products', final_title))
+
+# cleanup
